819637bd10da7c2b868f7656d9cf9b2600746e6b
PabloCruzadoG <nelcruzado2004@gmail.com>
2025-11-09 18:25:00 -0500

    mejora de dashboards

--- src/app/core/config/environment.ts
+++ src/app/core/config/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
 
-  apiCore: 'https://jcvjorge-transport-api-4b70ae842c45.herokuapp.com/api',
-  //apiCore: 'http://localhost:8080/api',
+  //apiCore: 'https://jcvjorge-transport-api-4b70ae842c45.herokuapp.com/api',
+  apiCore: 'http://localhost:8080/api',
 };
